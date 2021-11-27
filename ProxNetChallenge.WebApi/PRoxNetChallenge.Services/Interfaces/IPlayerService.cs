9c084bade99dd386d4acd77b442b0a1d327134c8
npozharskyi <Nikita.Pozharskyi@dataart.com>
2021-11-27 13:08:43 +0200

    add player service

--- ProxNetChallenge.WebApi/PRoxNetChallenge.Services/Interfaces/IPlayerService.cs
+++ ProxNetChallenge.WebApi/PRoxNetChallenge.Services/Interfaces/IPlayerService.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Threading.Tasks;
 using ProxNetChallenge.Entities;
+using ProxNetChallenge.Entities.models;
 
 namespace ProxNetChallenge.Services.Interfaces
 {
     public interface IPlayerService
     {
+        Task AddPlayer(string playerName, Vehicle vehicleType);
+        Task RemovePlayer(Guid id);
+        Task RemovePlayer(string playerName);
+        Task UpdatePlayer(PlayerEntity player);
+        Task<PlayerEntity> GetPlayer(Guid id);
+        Task<PlayerEntity> GetPlayer(string playerName);
 
     }
 }
