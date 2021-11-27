608a93bbac34a8813369e2dda8702d03821eb966
npozharskyi <Nikita.Pozharskyi@dataart.com>
2021-11-27 12:52:02 +0200

    add lobby service

---
+++ ProxNetChallenge.WebApi/PRoxNetChallenge.Services/LobbyService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProxNetChallenge.Entities;
+using ProxNetChallenge.Repository.Interfaces;
+using ProxNetChallenge.Services.Interfaces;
+
+namespace ProxNetChallenge.Services
+{
+    public class LobbyService :ILobbyService
+    {
+        private readonly IPlayerRepository _playerRepository;
+        private readonly ILobbyPlayerRepository _lobbyPlayerRepository;
+
+        public LobbyService(IPlayerRepository playerRepository, ILobbyPlayerRepository lobbyPlayerRepository)
+        {
+            _playerRepository = playerRepository;
+            _lobbyPlayerRepository = lobbyPlayerRepository;
+        }
+
+        public async Task AddPlayerToLobby(Guid id)
+        {
+            var entity = await _playerRepository.GetByIdAsync(id);
+            if (entity == null)
+            {
+                throw new InvalidDataException("No user in base");
+            }
+
+            await _lobbyPlayerRepository.AddAsync(new LobbyPlayerEntity
+            {
+                PlayerId = entity.Id,
+                VehicleType = entity.VehicleType,
+                IncomeDate = DateTime.Now
+            });
+        }
+    }
+}
--- ProxNetChallenge.WebApi/ProxNetChallenge.WebApi/Controllers/LobbyController.cs
+++ ProxNetChallenge.WebApi/ProxNetChallenge.WebApi/Controllers/LobbyController.cs
@@ -4,21 +4,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ProxNetChallenge.Entities.models;
+using ProxNetChallenge.Services;
+using ProxNetChallenge.Services.Interfaces;
 
 namespace ProxNetChallenge.WebApi.Controllers
 {
     [Route("api/v1/lobby")]
     public class LobbyController : Controller
     {
+        private readonly IPlayerService _playerService;
+        public LobbyController(IPlayerService playerService)
+        {
+            _playerService = playerService;
+        }
+
         [HttpGet()]
         public IActionResult Lobby(string userName, Vehicle vehicle)
         {
             return Ok();
         }
 
-        public Task<IActionResult> AddPlayerToLobby()
+        [HttpPost("player")]
+        public Task<IActionResult> AddPlayerToLobby(Guid id)
         {
-
+            _playerService
         }
     }
 }
